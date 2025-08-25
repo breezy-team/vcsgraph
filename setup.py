335ef639c4e11c5738be492002c33de9c0e48895
Jelmer Vernooĳ <jelmer@jelmer.uk>
2025-08-26 00:02:05 +0100

    Fix CI build

--- setup.py
+++ setup.py
@@ -2,5 +2,12 @@
 """Setup script for vcsgraph package."""
 
 from setuptools import setup
+from setuptools_rust import Binding, RustExtension
 
-setup()
+setup(
+    rust_extensions=[
+        RustExtension(
+            "vcsgraph._graph_rs", "crates/graph-py/Cargo.toml", binding=Binding.PyO3
+        ),
+    ]
+)
